84db981ee9e26f96776030ac3a855a8d3d354789
Hari <hariharan.udhayaravi@gmail.com>
2025-05-05 16:49:01 +0100

    Added default python file

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,10 @@
 # Need to install following pip install pypdf2 langchain faiss-cpu openai tiktoken streamlit
 # to run locally : streamlit run C:\Users\harih\PycharmProjects\GenAiExample\chatbot.py
 
-
 import streamlit as st
 
 st.header("My First Chatbot")
 
 with st.sidebar:
-        st.title("My First Chatbot")
+        st.title("My First Chatbot Attempt")
         file = st.file_uploader("Upload a file", type = "pdf")
